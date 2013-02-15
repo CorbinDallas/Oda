a3aa227289c41dc5c03814b734031525dd66f105
Tony Germaneri <tonygermaneri@gmail.com>
2013-02-14 23:49:14 -0800

    Finished Oda.Ajax function

--- Oda/Oda.UI/js/UI/Ajax.js
+++ Oda/Oda.UI/js/UI/Ajax.js
@@ -19,14 +19,91 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
  * OR OTHER DEALINGS IN THE SOFTWARE.
 */
-/**
-* Causes one object to inherit another as a prototype.
-* @function 
-* @name Oda.beget
-* @param {Native.String} [o] The object to inherit.
-* @returns {Native.Object} The new object.
-*/
-Oda.Ajax = function (args) {
-    args = args || { };
-
+Oda.Ajax = function (args, options) {
+    // map is defined as one of these two patterns
+    //
+    //    {
+    //        method: '',
+    //        parameters: [],
+    //        procedure: function(thisResult, allResults){ },
+    //        context: this
+    //    }
+    //
+    //    or...
+    //   
+    //    [
+    //        {
+    //            method: '',
+    //            parameters: [],
+    //            procedure: function(thisResult, allResults){ },
+    //            context: this
+    //        },
+    //        <additonal request blocks>
+    //    ]
+    //
+    //
+    Oda.assert(args !== undefined, 'Oda.Ajax missing parameter.');
+    var self = {};
+    options = options || { };
+    var createRequest = function() {
+        try { return new XMLHttpRequest(); } catch(e) {}
+        try { return new ActiveXObject('Msxml2.XMLHTTP'); } catch(e) {}
+        try { return new ActiveXObject('Microsoft.XMLHTTP'); } catch(e) {}
+        return null;
+    };
+    self.beginRequest = function () {
+        self.map = [];
+        self.methodInstances = { };
+        if(Oda.isArray(args)) {
+            // array of methods
+            for (var x = 0; args.length > x; x++) {
+                // assign this method an instance number in case it occurs more than once
+                if (self.methodInstances[args[x].method] === undefined) {
+                    self.methodInstances[args[x].method] = 1;
+                }else {
+                    self.methodInstances[args[x].method]++;
+                }
+                self.map.push([
+                    args[x].method,
+                    args[x].parameters
+                ]);
+                // assgin the instance number back to the mapper object
+                args[x].instanceNumber = self.methodInstances[args[x].method];
+            }
+        } else {
+            // a single method
+            self.map.push([
+                args.method,
+                args.parameters
+            ]);
+        }
+        self.id = Oda.createUUID();
+        self.request = createRequest();
+        self.request.onreadystatechange = self.readyStateChange;
+        self.request.open(options.method || 'POST', (options.responderUrl || '/responder?id=') + self.id, options.async || true);
+        self.request.setRequestHeader('Content-Type', options.contentType || 'application/json; charset=utf-8');
+        self.request.send(JSON.stringify(self.map));
+    };
+    self.readyStateChange = function() {
+        if (self.request.readyState != 4 || self.request.status == 0) { return; }
+        if (self.request.status != 200) {
+            // an error occured
+            Oda.UI.Widget.log('Oda.Ajax request returned a status of ' + self.request.status + ':\n' + self.request.responseText);
+            return;
+        }
+        var allResults = JSON.parse(self.request.responseText);
+        // seperate responses into map collection and call callback functions
+        if (Oda.isArray(args)) {
+            // array of methods
+            for (var x = 0; args.length > x; x++) {
+                var instanceNumber = self.methodInstances[args[x].method] > 1 ? '_' + args[x].instanceNumber : '';
+                args[x].procedure.apply(args[x].context, [allResults[args[x].method + instanceNumber], allResults]);
+            }
+        } else {
+            // a single method
+            args.procedure.apply(args.context, [allResults[args.method], allResults]);
+        }
+    };
+    self.beginRequest();
+    return self;
 }
